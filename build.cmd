04d276e844e557d93600068ed5ec74a341e66427
Damon Barry <damonb@microsoft.com>
2018-09-17 10:10:45 -0700

    Move client/native to client/c

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ if /i "%1" == "release" set build_config=Release
 msbuild server\c\server.vcxproj /p:Configuration=%build_config%;Platform=x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild client\native\client.vcxproj /p:Configuration=%build_config%;Platform=x64
+msbuild client\c\client.vcxproj /p:Configuration=%build_config%;Platform=x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet publish client\dotnet\client.csproj -c %build_config% -f netcoreapp2.1 -r win10-x64
